e5daad08f7644ca064584c259e604b721f565167
FernandoCagale <fernando.b.cagale@gmail.com>
2020-02-26 19:59:15 -0300

    add health to rabbitmq

--- api/handlers/health.go
+++ api/handlers/health.go
@@ -1,17 +1,30 @@
 package handlers
 
 import (
-	"github.com/FernandoCagale/c4-ecommerce/api/render"
+	"github.com/hellofresh/health-go"
+	"os"
+
+	healthRabbitmq "github.com/hellofresh/health-go/checks/rabbitmq"
+
 	"net/http"
+	"time"
 )
 
 type HealthHandler struct {
 }
 
-func (handler *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
-	render.Response(w, map[string]bool{"ok": true}, http.StatusOK)
-}
-
 func NewHealth() *HealthHandler {
 	return &HealthHandler{}
+}
+
+func (handler *HealthHandler) Health(w http.ResponseWriter, r *http.Request) {
+	health.Register(health.Config{
+		Name:    "rabbitmq",
+		Timeout: time.Second * 5,
+		Check: healthRabbitmq.New(healthRabbitmq.Config{
+			DSN: os.Getenv("AMQP_URI"),
+		}),
+	})
+
+	health.HandlerFunc(w, r)
 }
--- go.mod
+++ go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/go-ozzo/ozzo-validation v3.6.0+incompatible
 	github.com/google/wire v0.3.0
 	github.com/gorilla/mux v1.7.3
+	github.com/hellofresh/health-go v2.0.2+incompatible
 	github.com/joho/godotenv v1.3.0
 	github.com/stretchr/testify v1.4.0 // indirect
 )
